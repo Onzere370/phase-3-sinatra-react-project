70adc52bc0f71ef71d2e5f8ff2022cc720409d5b
cynthia <cynthiakagz@gmail.com>
2023-07-27 14:14:14 +0300

    update application controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
 
-class ProductsController < ApplicationController
-    def index
-      @products = Product.all
-      render json: @products
-    end
+# class ProductsController < ApplicationController
+#     def index
+#       @products = Product.all
+#       render json: @products
+#     end
   
-  end
+#   end
